555a5211a98951ccad55226cc20fd7f9c3c7daf4
Nguyen Dinh Hung <matrix16hung@gmail.com>
2025-01-31 00:30:17 +0300

    dynamic programing ex22

---
+++ Dynamic Programming/22.Sum_substring/main.cpp
@@ -0,0 +1,27 @@
+// Bài 22. Sum substring
+
+// Cho một số tự nhiên N được biểu diễn như một xâu kí tự, bạn hãy tính tổng của 
+// tất cả các số tạo bởi các xâu con liên tiếp của N, ví dụ N = 235 thì ta có tổng = 2 
+// + 3 + 5 + 23 + 35 + 235.
+
+// Input Format
+// Dòng duy nhất chứa số nguyên dương N
+
+// Constraints
+// 1<=N<=10^12
+
+// Output Format
+// In ra kết quả của bài toán
+
+// Sample Input 0
+// 1807
+// Sample Output 0
+// 2915
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  return 0;
+}
