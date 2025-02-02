c35a6d9d2895491d2bcfb17e40d4e7dec5df5d4c
Nguyen Dinh Hung <matrix16hung@gmail.com>
2025-02-02 23:51:34 +0300

    graph ex11

---
+++ Graph, DFS and BFS/12.BFS_on_directed_graphs/main.cpp
@@ -0,0 +1,38 @@
+// Bài 12. BFS trên đồ thị có hướng
+// Cho đồ thị có hướng G = (V, E) được biểu diễn dưới dạng danh sách cạnh. Hãy 
+// thực hiện in ra danh sách các đỉnh được duyệt theo thuật toán BFS(s).
+
+// Input Format
+// Dòng đầu tiên là 2 số n và m và s, tương ứng với số lượng đỉnh, cạnh của đồ thị 
+// và đỉnh bắt đầu duyệt. Các đỉnh của đồ thị được đánh số từ 1 tới n. m dòng tiếp 
+// theo mỗi dòng chứa đỉnh u, v (u != v) tương ứng với một cạnh của đồ thị.
+
+// Constraints
+// 1<=s<=n<=1000; 1<=m<=n*(n-1)/2;
+
+// Output Format
+// In ra các đỉnh được duyệt theo thuật toán BFS(s). Chú ý trong quá trình mở rộng 
+// các đỉnh của thuật toán BFS luôn lựa chọn duyệt các đỉnh có thứ tự nhỏ hơn 
+// trước.
+
+// Sample Input 0
+// 5 9 4
+// 5 1
+// 4 5
+// 2 5
+// 4 3
+// 1 4
+// 2 4
+// 5 3
+// 1 5
+// 1 2
+// Sample Output 0
+// 4 3 5 1 2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  return 0;
+}
